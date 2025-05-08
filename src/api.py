e6dbe6d87e7f97e6fc9cd922ad9b6d9fd2fe58a0
caviri <45425937+caviri@users.noreply.github.com>
2025-05-08 12:05:21 +0200

    feat: Basic API implementation

---
+++ src/api.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+import os
+from .core.gimie_methods import extract_gimie
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {"title": "Hello, welcome to the Git Metadata Extractor v0.0.1. Gimie Version 0.7.2. "}
+
+@app.get("/v1/extract/{full_path:path}")
+async def extract(full_path:str):
+    # First Gimie
+
+    # LLM
+    
+    pass
+    
+@app.get("/v1/gimie/{full_path:path}")
+async def gimie(full_path:str, 
+                format:str = "jsonld", 
+                serialize:bool = True):
+    try:
+        result = extract_gimie(full_path, format=format, serialize=serialize)
+        return result
+    except Exception as e:
+        return {"link": full_path, "output": str(e)}
+    
+
+@app.get("/v1/llm/{full_path:path}")
+async def llm(full_path:str):
+    # Only LLM
+    pass
+
+@app.exception_handler(ValueError)
+async def value_error_exception_handler(request: Request, exc: ValueError):
+    return JSONResponse(
+        status_code=400,
+        content={"message": str(exc)},
+    )
---
+++ src/core/gimie_methods.py
@@ -0,0 +1,33 @@
+from gimie.project import Project
+import json
+
+def extract_gimie(full_path: str, format: str = "jsonld", serialize: bool = True):
+    """
+    Extracts the GIMIE project from the given path.
+    
+    Args:
+        full_path (str): The full path to the GIMIE project.
+        format (str): The format to serialize the graph. Default is 'jsonld'.
+        
+    Returns:
+        Project: The GIMIE project object.
+    """
+
+    proj = Project(full_path)
+
+    # To retrieve the rdflib.Graph object
+    g = proj.extract()
+
+
+
+    if serialize:
+        # To retrieve the serialized graph
+        output = g.serialize(format=format)
+    else:
+        output = json.loads(g.serialize(format=format, encoding="utf-8"))
+
+    if output is None:
+        return None
+    else:
+        return {"link": full_path, "output": output}
+    
