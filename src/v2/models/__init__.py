a0a9dc1e530385c28d15b041456df1fb33f76b8c
Carlos Vivar Rios <carlosvivarrios@gmail.com>
2026-02-23 22:53:24 +0000

    feat(v2): add P1-06 error models

---
+++ src/v2/models/__init__.py
@@ -0,0 +1,17 @@
+from src.v2.models.contracts import (
+    V2ExtractResponse,
+    V2GraphResponse,
+    V2GraphUpdate,
+    V2Stats,
+)
+from src.v2.models.errors import V2ErrorResponse, V2ErrorType, V2FieldError
+
+__all__ = [
+    "V2ErrorResponse",
+    "V2ErrorType",
+    "V2ExtractResponse",
+    "V2FieldError",
+    "V2GraphResponse",
+    "V2GraphUpdate",
+    "V2Stats",
+]
